9f67535dedf03fcf11bb6faef03bbbc5f56c7e15
raamkashyap <ramkashyap26@gmail.com>
2019-04-30 19:52:24 +0530

    deleted duplicate logs

--- src/main/java/org/iiitb/BarcodeReader/MyResource.java
+++ src/main/java/org/iiitb/BarcodeReader/MyResource.java
@@ -30,8 +30,9 @@ import org.json.JSONObject;
  */
 @Path("myresource")
 public class MyResource {
-	private static final String SUCCESS_RESULT="Success";
-	private static final String FAILURE_RESULT="Failure";	
+        private String temp="";
+        private static final String SUCCESS_RESULT="Success";
+        private static final String FAILURE_RESULT="Failure";   
 
     /**
      * Method handling HTTP GET requests. The returned object will be sent
@@ -39,115 +40,115 @@ public class MyResource {
      *
      * @return String that will be returned as a text/plain response.
      */
-	@Path("userInfo")
+        @Path("userInfo")
     @POST
     @Produces(MediaType.APPLICATION_JSON)
     public String userInfo(String request) throws Exception{
-		System.out.println("Hello");
-		logs l = new logs();
-		Logger logger = l.getlogger();
-    	JSONObject data = new JSONObject(request);
-    	String roll_no = data.getString("roll_no");
-    	String fTime = data.getString("fTime");
-    	String date = data.getString("date");
-    	
-    	DatabaseConnection dc = new DatabaseConnection();
-    	dc.setDate(date);
-    	dc.setfTime(fTime);
-    	
-    	logs.logger.info("Successfully added student info " + date);
-    	
-    	return dc.userDetails(roll_no);
+                System.out.println("Hello");
+                logs l = new logs();
+                Logger logger = l.getlogger();
+         JSONObject data = new JSONObject(request);
+         String roll_no = data.getString("roll_no");
+         String fTime = data.getString("fTime");
+         String date = data.getString("date");
+         
+         DatabaseConnection dc = new DatabaseConnection();
+         dc.setDate(date);
+         dc.setfTime(fTime);
+         logs.logger.info("Successfully added student info " + date);
+         logs.removeDuplicates();
+         
+         return dc.userDetails(roll_no);
     }
-	
-	@Path("refundInfo")
+        
+        @Path("refundInfo")
     @POST
     @Produces(MediaType.APPLICATION_JSON)
     public String refundInfo(String request) throws Exception{
       
-	
-    	JSONObject data = new JSONObject(request);
-    	String roll_no = data.getString("roll_no");
-    	
-    	DatabaseConnection dc = new DatabaseConnection();
-    	return dc.refundDetails(roll_no);
+        
+         JSONObject data = new JSONObject(request);
+         String roll_no = data.getString("roll_no");
+         
+         DatabaseConnection dc = new DatabaseConnection();
+         return dc.refundDetails(roll_no);
     }
-	@Path("dashboard")
-	@GET
-	@Produces(MediaType.APPLICATION_JSON)
-	public String dashboard() throws Exception{
-		
-		DatabaseConnection dc = new DatabaseConnection();
-		ArrayList<register> registers = dc.dashBoard();
-		
-		JSONArray register_json_array = new JSONArray();
-		for(register item : registers) {
-			JSONObject register_json = new JSONObject();
-			register_json.put("rollNum", item.getRollNum());
-			register_json.put("date", item.getdate());
-			register_json.put("bf", item.getbf());
-			register_json.put("lunch", item.getlunch());
-			register_json.put("dinner", item.getdinner());
-			register_json_array.put(register_json);
-		}
-		JSONObject result = new JSONObject();
-		
-		result.put("items", register_json_array);
-		System.out.print(register_json_array.toString());
-		return register_json_array.toString();
-    	
-	}
-	@Path("/history/{roll_no}")
-	@GET
-	@Produces(MediaType.APPLICATION_JSON)
-	public String history(@PathParam("roll_no") String roll_no) throws Exception{
-		
-		System.out.println(roll_no);
-		DatabaseConnection dc = new DatabaseConnection();
-		ArrayList<register> registers = dc.get_Top5_Items(roll_no);
-		
-		JSONArray register_json_array = new JSONArray();
-		for(register item : registers) {
-			JSONObject register_json = new JSONObject();
-			register_json.put("rollNum", item.getRollNum());
-			register_json.put("date", item.getdate());
-			register_json.put("bf", item.getbf());
-			register_json.put("lunch", item.getlunch());
-			register_json.put("dinner", item.getdinner());
-			register_json_array.put(register_json);
-		}
-		JSONObject result = new JSONObject();
-		
-		result.put("items", register_json_array);
-		System.out.print(register_json_array.toString());
-		return register_json_array.toString();
-    	
-	}
-	@Path("charges")
-	@GET
-	@Produces(MediaType.APPLICATION_JSON)
-	public String charges() throws Exception{
-		DatabaseConnection dc = new DatabaseConnection();
-		return  dc.getcharges();
-	}
-	
-	@Path("updatecharges")
-	@POST
-	@Produces(MediaType.APPLICATION_JSON)
-	public String update(String request) throws Exception{
-		System.out.println("kfjggnf");
-		System.out.println(request);
-		JSONObject data = new JSONObject(request);
-    	int bcharge= data.getInt("bfcharge");
-    	int lcharge = data.getInt("lcharge");
-    	int dcharge = data.getInt("dcharge");
-    	int semcharge = data.getInt("semcharge");
-    	System.out.println("ksjdnkjdn "+ bcharge);
-    	
-    	DatabaseConnection dc = new DatabaseConnection();
-    	boolean result = dc.UpdateCharges(bcharge,lcharge,dcharge,semcharge);
-    	return result ? "{ \"Response\" : \"" + SUCCESS_RESULT + "\" }" : "{ \"Response\" : \"" + FAILURE_RESULT + "\" }";
-    		
-    	
-	}
+        @Path("dashboard")
+        @GET
+        @Produces(MediaType.APPLICATION_JSON)
+        public String dashboard() throws Exception{
+                
+                DatabaseConnection dc = new DatabaseConnection();
+                ArrayList<register> registers = dc.dashBoard();
+                
+                JSONArray register_json_array = new JSONArray();
+                for(register item : registers) {
+                        JSONObject register_json = new JSONObject();
+                        register_json.put("rollNum", item.getRollNum());
+                        register_json.put("date", item.getdate());
+                        register_json.put("bf", item.getbf());
+                        register_json.put("lunch", item.getlunch());
+                        register_json.put("dinner", item.getdinner());
+                        register_json_array.put(register_json);
+                }
+                JSONObject result = new JSONObject();
+                
+                result.put("items", register_json_array);
+                System.out.print(register_json_array.toString());
+                return register_json_array.toString();
+         
+        }
+        @Path("/history/{roll_no}")
+        @GET
+        @Produces(MediaType.APPLICATION_JSON)
+        public String history(@PathParam("roll_no") String roll_no) throws Exception{
+                
+                System.out.println(roll_no);
+                DatabaseConnection dc = new DatabaseConnection();
+                ArrayList<register> registers = dc.get_Top5_Items(roll_no);
+                
+                JSONArray register_json_array = new JSONArray();
+                for(register item : registers) {
+                        JSONObject register_json = new JSONObject();
+                        register_json.put("rollNum", item.getRollNum());
+                        register_json.put("date", item.getdate());
+                        register_json.put("bf", item.getbf());
+                        register_json.put("lunch", item.getlunch());
+                        register_json.put("dinner", item.getdinner());
+                        register_json_array.put(register_json);
+                }
+                JSONObject result = new JSONObject();
+                
+                result.put("items", register_json_array);
+                System.out.print(register_json_array.toString());
+                return register_json_array.toString();
+         
+        }
+        @Path("charges")
+        @GET
+        @Produces(MediaType.APPLICATION_JSON)
+        public String charges() throws Exception{
+                DatabaseConnection dc = new DatabaseConnection();
+                return  dc.getcharges();
+        }
+        
+        @Path("updatecharges")
+        @POST
+        @Produces(MediaType.APPLICATION_JSON)
+        public String update(String request) throws Exception{
+                System.out.println("kfjggnf");
+                System.out.println(request);
+                JSONObject data = new JSONObject(request);
+         int bcharge= data.getInt("bfcharge");
+         int lcharge = data.getInt("lcharge");
+         int dcharge = data.getInt("dcharge");
+         int semcharge = data.getInt("semcharge");
+         System.out.println("ksjdnkjdn "+ bcharge);
+         
+         DatabaseConnection dc = new DatabaseConnection();
+         boolean result = dc.UpdateCharges(bcharge,lcharge,dcharge,semcharge);
+         return result ? "{ \"Response\" : \"" + SUCCESS_RESULT + "\" }" : "{ \"Response\" : \"" + FAILURE_RESULT + "\" }";
+                 
+         
+        }
 }
--- src/main/java/org/iiitb/logs/logs.java
+++ src/main/java/org/iiitb/logs/logs.java
@@ -4,8 +4,11 @@ import org.apache.log4j.Logger;
 import org.apache.log4j.PatternLayout;
 import org.apache.log4j.RollingFileAppender;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 import org.apache.log4j.BasicConfigurator;
 import org.apache.log4j.Level;
@@ -17,30 +20,82 @@ import java.net.URL;
 
 
 public class logs {
-	public static Logger logger;
-	public logs()
-	{
-		
-		try {
-			logger=getlogger();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		
-		
-	}
-	
-	
-	public static Logger getlogger() throws IOException  {
-		RollingFileAppender appender =   new RollingFileAppender(new PatternLayout("%d{dd/MMM/YYYY:HH:mm:ss }  %m%n"), "/tmp/logs.out", true);
-	
+        public static Logger logger;
+        public logs()
+        {
+                
+                try {
+                        logger=getlogger();
+                } catch (IOException e) {
+                        e.printStackTrace();
+                }
+                
+                
+        }
+        
+        
+        public static Logger getlogger() throws IOException  {
+                RollingFileAppender appender =   new RollingFileAppender(new PatternLayout("%d{dd/MMM/YYYY:HH:mm:ss }  %m%n"), "/tmp/input.out", true);
+        
 
-		appender.setMaxBackupIndex(5);
-		appender.setMaxFileSize("1MB");
-		Logger logger = Logger.getRootLogger();
-		logger.setLevel(Level.DEBUG);
-		logger.addAppender(appender);
-		return logger;
-	}
-	
-}
+                appender.setMaxBackupIndex(5);
+                appender.setMaxFileSize("1MB");
+                Logger logger = Logger.getRootLogger();
+                logger.setLevel(Level.DEBUG);
+                //logger.setAdditivity(false);
+                logger.addAppender(appender);
+                return logger;
+        }
+        public static void removeDuplicates() throws IOException { 
+                // PrintWriter object for output.txt 
+                PrintWriter pw = new PrintWriter("/tmp/output.out"); 
+                  
+                // BufferedReader object for input.txt 
+                BufferedReader br1 = new BufferedReader(new FileReader("/tmp/input.out")); 
+                  
+                String line1 = br1.readLine(); 
+                  
+                // loop for each line of input.txt 
+                while(line1 != null) 
+                { 
+                    boolean flag = false; 
+                      
+                    // BufferedReader object for output.txt 
+                    BufferedReader br2 = new BufferedReader(new FileReader("/tmp/output.out")); 
+                      
+                    String line2 = br2.readLine(); 
+                      
+                    // loop for each line of output.txt 
+                    while(line2 != null) 
+                    { 
+                          
+                        if(line1.equals(line2)) 
+                        { 
+                            flag = true; 
+                            break; 
+                        } 
+                          
+                        line2 = br2.readLine(); 
+                      
+                    } 
+                      
+                    // if flag = false 
+                    // write line of input.txt to output.txt 
+                    if(!flag){ 
+                        pw.println(line1); 
+                          
+                        // flushing is important here 
+                        pw.flush(); 
+                    } 
+                      
+                    line1 = br1.readLine(); 
+                      
+                } 
+                  
+                // closing resources 
+                br1.close(); 
+                pw.close(); 
+                  
+                System.out.println("File operation performed successfully"); 
+            } 
+        }
