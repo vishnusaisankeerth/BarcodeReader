4d71641a6e7cb663ab420ee54d102127eed819f9
raamkashyap <ramkashyap26@gmail.com>
2019-04-30 21:56:17 +0530

    changed logs schema

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -3,6 +3,8 @@ import static org.junit.Assert.*;
 import org.iiitb.BarcodeReader.DatabaseConnection;
 import org.iiitb.model.register;
 import org.junit.Test;
+import org.json.JSONObject;
+import org.json.JSONException;
 
 public class MainTest {
 
@@ -35,5 +37,38 @@ public class MainTest {
 		original = dc.get_student_date_status("IMT2015510","2019-04-24");
 		System.out.println(dummy_register.getbf());
 		assertEquals(dummy_register.getlunch(),original.getlunch());
+	}
+	@Test
+	public void TestDinner() {
+		register dummy_register = new register();
+		register original = new register();
+		dummy_register.setbf("No");
+		dummy_register.setlunch("No");
+		dummy_register.setdinner("Yes");
+		dummy_register.setRollNum("IMT2015510");
+		dummy_register.setDate("24-04-2019");
+				
+		DatabaseConnection dc = new DatabaseConnection();
+		original = dc.get_student_date_status("IMT2015510","2019-04-24");
+		System.out.println(dummy_register.getbf());
+		assertEquals(dummy_register.getdinner(),original.getdinner());
+	}
+	@Test
+	public void TestBfCharge() throws JSONException{
+		int bfCharge = 40;
+		
+		DatabaseConnection dc = new DatabaseConnection();
+		String original = dc.getcharges();
+		JSONObject json = new JSONObject(original);
+		assertEquals(bfCharge, json.getInt("bf"));
+	}
+	@Test
+	public void TestSemCharge() throws JSONException{
+		int semCharge = 33000;
+		
+		DatabaseConnection dc = new DatabaseConnection();
+		String original = dc.getcharges();
+		JSONObject json = new JSONObject(original);
+		assertEquals(semCharge, json.getInt("sem"));
 	}
 }
